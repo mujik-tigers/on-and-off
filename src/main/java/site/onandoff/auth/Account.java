ed56ea5914fa0db786387d602767e1fcfab6456a
yeonise <cerealnum@gmail.com>
2023-11-29 10:38:19 +0900

    feat: 로그인한 사용자의 정보를 담는 Account 생성
    - id: 사용자의 고유 식별 아이디

---
+++ src/main/java/site/onandoff/auth/Account.java
@@ -0,0 +1,14 @@
+package site.onandoff.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Account {
+
+	private long id;
+
+}
