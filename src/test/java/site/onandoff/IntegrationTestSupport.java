422a851ee2187172ce7eecc7f46d88616896e3b4
yeonise <cerealnum@gmail.com>
2023-11-29 20:25:39 +0900

    test: schema.sql을 사용한 초기화 제거

--- src/test/java/site/onandoff/IntegrationTestSupport.java
+++ src/test/java/site/onandoff/IntegrationTestSupport.java
@@ -8,7 +8,6 @@ import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,7 +17,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootTest
 @ActiveProfiles("test")
 @ExtendWith(RestDocumentationExtension.class)
-@Sql(value = "classpath:schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public abstract class IntegrationTestSupport {
 
 	@Autowired
