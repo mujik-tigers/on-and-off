e6cb04bf4336a60026981358d660c8981f18d141
yeonise <cerealnum@gmail.com>
2023-11-29 10:41:24 +0900

    feat: [config] interceptor와 resolver 등록

---
+++ src/main/java/site/onandoff/config/WebConfig.java
@@ -0,0 +1,49 @@
+package site.onandoff.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+import site.onandoff.interceptor.AccessTokenInterceptor;
+import site.onandoff.interceptor.RefreshTokenInterceptor;
+import site.onandoff.util.LoginArgumentResolver;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+	private final AccessTokenInterceptor accessTokenInterceptor;
+	private final RefreshTokenInterceptor refreshTokenInterceptor;
+	private final LoginArgumentResolver loginArgumentResolver;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods(
+				HttpMethod.HEAD.name(), HttpMethod.GET.name(), HttpMethod.POST.name(),
+				HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(),
+				HttpMethod.OPTIONS.name());
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(accessTokenInterceptor)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/reissue");
+		registry.addInterceptor(refreshTokenInterceptor)
+			.addPathPatterns("/reissue");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginArgumentResolver);
+	}
+
+}
